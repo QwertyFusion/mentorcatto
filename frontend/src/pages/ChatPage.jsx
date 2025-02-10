e742b84b29926d47cdf141e039058ed3db5c71ca
Rishi Banerjee <rishibanerjee10@gmail.com>
2025-02-11 01:20:13 +0530

    Responsive navbar

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -2,15 +2,12 @@ import LeftNavbar from "../components/LeftNavbar";
 
 const ChatPage = () => {
   return (
-    <div className='h-screen w-full grid grid-cols-12'>
-
-      <div className='col-span-2'>
-
+    <div className='h-screen w-full flex'>
+      <div className='min-w-[250px]'>
         <LeftNavbar />
       </div>
 
-      <div className='col-span-10 bg-gray-100'>
-
+      <div className='flex-1 bg-gray-100'>
         <h2 className='p-4'>Right Column</h2>
       </div>
     </div>
