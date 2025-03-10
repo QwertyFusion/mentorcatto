6f25b8bbacadea90a389e5be30a54e4dc978fc7e
Ayush Ranjan <ranjanayush881@gmail.com>
2025-03-10 23:53:50 +0530

    added icons in icon store

--- backend/util/ai_agent.js
+++ backend/util/ai_agent.js
@@ -8,51 +8,51 @@ import { config } from "dotenv";
 config();
 
 const llm = new ChatGoogleGenerativeAI({
-    model: "gemini-2.0-flash",
-    apiKey: process.env.GEMINI_API_KEY,
+  model: "gemini-pro",
+  apiKey: process.env.GEMINI_API_KEY,
 });
 
 // Define tools
 const multiply = tool(
-    async ({ a, b }) => {
-        return `${a * b}`;
-    },
-    {
-        name: "multiply",
-        description: "Multiply two numbers together",
-        schema: z.object({
-            a: z.number().describe("first number"),
-            b: z.number().describe("second number"),
-        }),
-    }
+  async ({ a, b }) => {
+    return `${a * b}`;
+  },
+  {
+    name: "multiply",
+    description: "Multiply two numbers together",
+    schema: z.object({
+      a: z.number().describe("first number"),
+      b: z.number().describe("second number"),
+    }),
+  }
 );
 
 const add = tool(
-    async ({ a, b }) => {
-        return `${a + b}`;
-    },
-    {
-        name: "add",
-        description: "Add two numbers together",
-        schema: z.object({
-            a: z.number().describe("first number"),
-            b: z.number().describe("second number"),
-        }),
-    }
+  async ({ a, b }) => {
+    return `${a + b}`;
+  },
+  {
+    name: "add",
+    description: "Add two numbers together",
+    schema: z.object({
+      a: z.number().describe("first number"),
+      b: z.number().describe("second number"),
+    }),
+  }
 );
 
 const divide = tool(
-    async ({ a, b }) => {
-        return `${a / b}`;
-    },
-    {
-        name: "divide",
-        description: "Divide two numbers",
-        schema: z.object({
-            a: z.number().describe("first number"),
-            b: z.number().describe("second number"),
-        }),
-    }
+  async ({ a, b }) => {
+    return `${a / b}`;
+  },
+  {
+    name: "divide",
+    description: "Divide two numbers",
+    schema: z.object({
+      a: z.number().describe("first number"),
+      b: z.number().describe("second number"),
+    }),
+  }
 );
 
 // Augment the LLM with tools
@@ -62,82 +62,82 @@ const llmWithTools = llm.bindTools(tools);
 
 // Nodes
 async function llmCall(state) {
-    // LLM decides whether to call a tool or not
-    const result = await llmWithTools.invoke([
-        {
-            role: "system",
-            content: `You are a helpful AI assistant specialized in performing arithmetic operations on a set of inputs. When providing an answer, ensure that it is formatted as a complete sentence and includes the numerical result whenever possible.
+  // LLM decides whether to call a tool or not
+  const result = await llmWithTools.invoke([
+    {
+      role: "system",
+      content: `You are a helpful AI assistant specialized in performing arithmetic operations on a set of inputs. When providing an answer, ensure that it is formatted as a complete sentence and includes the numerical result whenever possible.
 * If the input contains a mathematical question, clearly explain the steps involved in solving it before presenting the final numerical result.
 * If the input does not allow for a numerical output, respond appropriately by providing a detailed explanation rather than simply stating that an answer is correct.
 * Avoid vague confirmations like "That's correct." Instead, focus on delivering a thorough and informative response that outlines the reasoning behind your answer.
 * Use the available tools to generate the most accurate and precise response.`,
-        },
-        ...state.messages,
-    ]);
+    },
+    ...state.messages,
+  ]);
 
-    return {
-        messages: [result],
-    };
+  return {
+    messages: [result],
+  };
 }
 
 async function toolNode(state) {
-    // Performs the tool call
-    const results = [];
-    const lastMessage = state.messages.at(-1);
+  // Performs the tool call
+  const results = [];
+  const lastMessage = state.messages.at(-1);
 
-    if (lastMessage?.tool_calls?.length) {
-        for (const toolCall of lastMessage.tool_calls) {
-            const tool = toolsByName[toolCall.name];
-            const observation = await tool.invoke(toolCall.args);
-            results.push(
-                new ToolMessage({
-                    content: observation,
-                    tool_call_id: toolCall.id,
-                })
-            );
-        }
+  if (lastMessage?.tool_calls?.length) {
+    for (const toolCall of lastMessage.tool_calls) {
+      const tool = toolsByName[toolCall.name];
+      const observation = await tool.invoke(toolCall.args);
+      results.push(
+        new ToolMessage({
+          content: observation,
+          tool_call_id: toolCall.id,
+        })
+      );
     }
+  }
 
-    return { messages: results };
+  return { messages: results };
 }
 
 // Conditional edge function to route to the tool node or end
 function shouldContinue(state) {
-    const messages = state.messages;
-    const lastMessage = messages.at(-1);
+  const messages = state.messages;
+  const lastMessage = messages.at(-1);
 
-    // If the LLM makes a tool call, then perform an action
-    if (lastMessage?.tool_calls?.length) {
-        return "Action";
-    }
-    // Otherwise, we stop (reply to the user)
-    return "__end__";
+  // If the LLM makes a tool call, then perform an action
+  if (lastMessage?.tool_calls?.length) {
+    return "Action";
+  }
+  // Otherwise, we stop (reply to the user)
+  return "__end__";
 }
 
 // Build workflow
 const agentBuilder = new StateGraph(MessagesAnnotation)
-    .addNode("llmCall", llmCall)
-    .addNode("tools", toolNode)
-    // Add edges to connect nodes
-    .addEdge("__start__", "llmCall")
-    .addConditionalEdges("llmCall", shouldContinue, {
-        // Name returned by shouldContinue : Name of next node to visit
-        Action: "tools",
-        __end__: "__end__",
-    })
-    .addEdge("tools", "llmCall")
-    .compile();
+  .addNode("llmCall", llmCall)
+  .addNode("tools", toolNode)
+  // Add edges to connect nodes
+  .addEdge("__start__", "llmCall")
+  .addConditionalEdges("llmCall", shouldContinue, {
+    // Name returned by shouldContinue : Name of next node to visit
+    Action: "tools",
+    __end__: "__end__",
+  })
+  .addEdge("tools", "llmCall")
+  .compile();
 
 // Invoke
 export async function invokeAgent(userMessage) {
-    const messages = [
-        {
-            role: "user",
-            content: userMessage,
-        },
-    ];
-    const result = await agentBuilder.invoke({ messages });
+  const messages = [
+    {
+      role: "user",
+      content: userMessage,
+    },
+  ];
+  const result = await agentBuilder.invoke({ messages });
 
-    // Convert the response into JSON format
-    return JSON.stringify({ response: result.messages }, null, 2);
+  // Convert the response into JSON format
+  return JSON.stringify({ response: result.messages }, null, 2);
 }
