a14f02b22b8f3defc78d174a8410fe4be70281d0
Rishi Banerjee <rishibanerjee10@gmail.com>
2025-03-10 23:49:58 +0530

    Course Side bar fix

--- frontend/src/pages/CoursePage.jsx
+++ frontend/src/pages/CoursePage.jsx
@@ -3,30 +3,30 @@ import { motion } from "framer-motion";
 import CoursesRightSideBar from "../components/CoursesRightSideBar";
 
 const CoursePage = () => {
-  return (
-    <div className="h-screen w-full flex">
-      <div className="min-w-[250px]">
-        <LeftNavbar />
-      </div>
+    return (
+        <div className="h-screen w-full flex">
+            <div className="w-fit">
+                <LeftNavbar />
+            </div>
 
-      <div className="flex-1 font-inter bg-accent-2 p-4 flex flex-col inner-shadow-custome">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.9 }}
-          transition={{ duration: 0.5 }}
-          className="max-w-md w-full mx-auto mt-10 p-8 bg-accent-4 bg-opacity-80 backdrop-filter backdrop-blur-lg rounded-xl shadow-2xl border border-gray-800"
-        >
-          <h2 className="text-3xl font-bold mb-6 text-center bg-primary text-transparent bg-clip-text">
-            Courses
-          </h2>
-        </motion.div>
-      </div>
-      <div className="min-w-[300px]">
-        <CoursesRightSideBar />
-      </div>
-    </div>
-  );
+            <div className="flex-1 font-inter bg-accent-2 p-4 flex flex-col">
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.9 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0.9 }}
+                    transition={{ duration: 0.5 }}
+                    className="max-w-md w-full mx-auto mt-10 p-8 bg-accent-4 bg-opacity-80 backdrop-filter backdrop-blur-lg rounded-xl shadow-2xl border border-gray-800"
+                >
+                    <h2 className="text-3xl font-bold mb-6 text-center bg-primary text-transparent bg-clip-text">
+                        Courses
+                    </h2>
+                </motion.div>
+            </div>
+            <div className="min-w-[350px]">
+                <CoursesRightSideBar />
+            </div>
+        </div>
+    );
 };
 
 export default CoursePage;
